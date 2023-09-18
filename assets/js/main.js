70441c38603317ccf9bd7b55ba16ad943f78e824
BatyaRoyale <saih.niyaz@gmail.com>
2023-09-18 08:29:15 +0300

    sm8

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,12 @@
-let back = document.querySelector('.back')
-let planet = document.querySelector('.animation-bg')
 let modal = document.querySelector('.btn-modal')
 let modalblock = document.getElementById('modal-block')
 let close = document.querySelector('.close-block')
+let over = document.querySelector('.mouseover')
+let move = document.querySelector('.mousemove')
+let mousup = document.querySelector('.mouseup')
+let mousedown = document.querySelector('.mousedown')
 
-
-
-
-back.addEventListener('click', function(){
-    planet.style.display = 'block';
-})
-
-modal.addEventListener('click', function(){
+modal.addEventListener('dblclick', function(){
     modalblock.style.left = '0';
     modalblock.style.display = 'flex';
     modalblock.style.justifyContent = 'center';
@@ -22,3 +17,43 @@ modal.addEventListener('click', function(){
 close.addEventListener('click', function(){
     modalblock.style.left = '-100vw';
 })
+over.addEventListener('mouseover', function(){
+    over.style.background = 'black';
+})
+over.addEventListener('mouseout', function(){
+    over.style.background = 'white';
+})
+
+
+const setBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    move.style.backgroundColor = "#" + randomColor;
+    modalblock.style.backgroundColor = "#" + randomColor;
+    move.style.height = "100vh";
+    move.style.width = "100vw";
+    move.style.cursor = "none";
+    document.documentElement.requestFullscreen();
+        const element = document.documentElement;
+        if(element.requestFullscreen){
+            element.requestFullscreen();
+        }else if (element.mozRequestFullScreen) {
+            element.mozRequestFullscreen();
+        }else if (element.webkitRequestFullScreen) {
+            element.webkitRequestFullscreen();
+        }else if (element.msRequestFullScreen) {
+            element.msRequestFullscreen();
+        }
+}
+
+// move.addEventListener('mousemove', )
+mousup.addEventListener('mouseup',function(){
+    mousup.style.height = "50px";
+    mousup.style.background = "yellow";
+} )
+
+mousedown.addEventListener('mousedown',function(){
+    mousedown.style.height = "50px";
+    mousedown.style.background = "blue";
+} )
+
+
